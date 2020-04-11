f94421cb5f5cec06ba1672bd25b4ef8a1d715a16
Shreyansh Khajanchi <shreyansh_k@live.com>
2020-04-11 17:46:21 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.rst", 'rt', encoding='utf8') as f:
 
 setup(
     name='Flask-Firehose',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/shreyanshk/flask-firehose',
     license='MIT',
     author='Shreyansh Khajanchi',
